ff65a9a5445ec10ee7051eb628164c18e08fc492
Jackelyn <jackelyngironvillacinda@gmail.com>
2025-08-29 08:07:22 -0600

    Se agrego TeamListScreen

--- app/src/main/java/com/uvg/teamapp/MainActivity.kt
+++ app/src/main/java/com/uvg/teamapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uvg.teamapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.uvg.teamapp.model.TeamMember;
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -20,17 +21,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.uvg.teamapp.repository.FakeTeamRepository
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             TeamAppTheme {
                 Surface(
@@ -43,7 +47,7 @@ class MainActivity : ComponentActivity() {
                     TeamListScreen(
                         teamMembers = teamMembers,
                         onMemberClick = { member ->
-                            println("Clickeado: ${member.name}")
+                            println("Click: ${member.name}")
                         }
                     )
                 }
@@ -68,7 +72,7 @@ fun TeamListScreen(
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(horizontal = 16.dp, vertical = 50.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
         items(teamMembers) { member ->
@@ -79,20 +83,28 @@ fun TeamListScreen(
                 elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
             ) {
                 Column(
-                    modifier = Modifier.padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .height(120.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
                     Text(
                         text = member.name,
                         style = MaterialTheme.typography.headlineSmall,
-                        fontWeight = FontWeight.Bold
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
                     )
 
                     Spacer(modifier = Modifier.height(8.dp))
+
                     Text(
                         text = member.description,
                         style = MaterialTheme.typography.bodyMedium,
-                        textAlign = TextAlign.Center
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth()
                     )
                 }
             }
